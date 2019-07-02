9b56ce427a5c254fb63510dd7fdecb8bef3a8ae8
Chitra Singla <chitrasingla2001@gmail.com>
2019-07-02 12:21:37 +0530

    adding starting project

---
+++ css/main.js
@@ -0,0 +1,7 @@
+let signbox = document.getElementById('signinpage');
+let sign = document.getElementById('signinbox');
+sign.onclick = function() {
+    let signin = signbox.nodeValue;
+    window.alert(signin);
+}
+
